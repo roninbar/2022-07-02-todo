f017251bc0719cd8ccff6dba8a7a9b3e2a262d4f
ron <rinbar70@gmail.com>
2022-06-29 21:42:42 +0300

    First commit.

---
+++ index.js
@@ -0,0 +1,43 @@
+function ToDoItem({ what, when, who, completed }) {
+    this.what = what;
+    this.when = when;
+    this.who = who;
+    this.completed = typeof completed === 'boolean' ? completed : false;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    /**
+     * @type HTMLInputElement
+     */
+    const inputWhat = document.getElementById('what');
+    /**
+     * @type HTMLInputElement
+     */
+    const inputWhen = document.getElementById('when');
+    /**
+     * @type HTMLInputElement
+     */
+    const inputWho = document.getElementById('who');
+
+    document.querySelector('form').addEventListener('submit', function (ev) {
+        ev.preventDefault();
+        const item = new ToDoItem({ what: inputWhat.value, when: inputWhen.value, who: inputWho.value });
+        inputWhat.value = inputWhen.value = inputWho.value = '';
+        inputWhat.focus();
+        const tr = document.createElement('tr');
+        tr.innerHTML = /*html*/`
+            <td>${item.what}</td>
+            <td>${item.when}</td>
+            <td>${item.who}</td>
+            <td><input type="checkbox"></td>`;
+        /**
+         * @type HTMLInputElement
+         */
+        const checkbox = tr.querySelector('input[type=checkbox]');
+        checkbox.addEventListener('change', function () {
+            tr.classList[this.checked ? 'add' : 'remove']('completed');
+        });
+        document.querySelector('tbody').appendChild(tr);
+    });
+});
